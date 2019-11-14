c17262944818670787693bc1322ba14b76eb6af6
Alan Ionita <a.g.ionita@gmail.com>
2019-11-14 01:17:13 +0000

    adds newProduct mutation

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -11,10 +11,14 @@ const productsTypeMatcher = {
 
 const products = () => Product.find({}).exec()
 const product = (_, args) => Product.findById(args.id).exec()
+const newProduct = (_, args, context) =>
+  Product.create({ ...args.input, createdBy: context.user._id })
 
 export default {
   Query: { products, product },
-  Mutation: {},
+  Mutation: {
+    newProduct
+  },
   Product: {
     __resolveType(product) {}
   }
