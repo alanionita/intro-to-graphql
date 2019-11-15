b2503b6c9f80b0285ea6714f0118ac60d01058a3
Alan Ionita <alan.ionita@codecomputerlove.com>
2019-11-15 13:50:29 +0000

    FIX : removes resolvers

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,32 +9,13 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
-const products = () => Product.find({}).exec()
-const product = (_, args) => Product.findById(args.id).exec()
-const newProduct = (_, args, context) =>
-  Product.create({ ...args.input, createdBy: context.user._id })
-const updateProduct = (_, args, context) =>
-  Product.findByIdAndUpdate(args.id, args.input, { new: true })
-    .lean()
-    .exec()
-const removeProduct = (_, args, context) =>
-  Product.findByIdAndRemove(args.id)
-    .lean()
-    .exec()
-
 export default {
-  Query: { products, product },
+  Query: {  },
   Mutation: {
-    newProduct,
-    updateProduct,
-    removeProduct
+    
   },
   Product: {
     __resolveType(product) {},
-    createdBy(product) {
-      return User.findById(product.createdBy)
-        .lean()
-        .exec()
-    }
+  
   }
 }
